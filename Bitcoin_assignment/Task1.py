31399cfcb2ee9d0a3dd595dd60c4dd6a479b272c
LeoVogiatzis <leovogiatzis@gmail.com>
2021-05-12 00:00:56 +0300

    Basic procedures for Task2 and Task1 completion

--- Bitcoin_assignment/Task1.py
+++ Bitcoin_assignment/Task1.py
@@ -1,6 +1,6 @@
 from bitcoinutils.setup import setup
-from bitcoinutils.transactions import Transaction, TxInput, TxOutput, Sequence
-from bitcoinutils.keys import P2pkhAddress, P2shAddress, PrivateKey
+from bitcoinutils.transactions import Transaction, TxInput, TxOutput, Locktime
+from bitcoinutils.keys import P2pkhAddress, P2shAddress, PrivateKey, PublicKey
 from bitcoinutils.script import Script
 from bitcoinutils.constants import TYPE_ABSOLUTE_TIMELOCK
 from bitcoinutils.proxy import NodeProxy
@@ -15,34 +15,27 @@ def main():
     """
     # setup regtest network
     setup('regtest')
-    priv = PrivateKey('')
+    # priv = PrivateKey('')
     parser = argparse.ArgumentParser(description='Bitcoin Script for Task I')
-    parser.add_argument('--public_key', type=int, help='Insert a public key')
-    parser.add_argument('--time_loc',
+    parser.add_argument('public_key', help='Insert a public key')
+    parser.add_argument('--time_loc', type=int,
                         help='Insert a parameter (block height/Unix Epoch time that funds should be locked')
 
     args = parser.parse_args()
     public_key = args.public_key
     absolute_param = args.time_loc
 
-    # Ask for public key /P2SH address
-
-
-
-    # set values
-    relative_blocks = 20
-
-    seq = Sequence(TYPE_ABSOLUTE_TIMELOCK, relative_blocks)
+    loc = Locktime(absolute_param)
 
     # secret key corresponding to the pubkey needed for the P2SH (P2PKH) transaction
-    p2pkh_sk = PrivateKey('cRvyLwCPLU88jsyj94L7iJjQX5C2f8koG4G2gevN4BeSGcEvfKe9')
+    p2pkh_sk = PublicKey(public_key)
 
     # get the address (from the public key)
-    p2pkh_addr = p2pkh_sk.get_public_key().get_address()
+    p2pkh_addr = p2pkh_sk.get_address()
 
     # create the redeem script
     redeem_script = Script(
-        [seq.for_script(), 'OP_CHECKLOCKTIMEVERIFY', 'OP_DROP', 'OP_DUP', 'OP_HASH160', p2pkh_addr.to_hash160(),
+        [absolute_param, 'OP_CHECKLOCKTIMEVERIFY', 'OP_DROP', 'OP_DUP', 'OP_HASH160', p2pkh_addr.to_hash160(),
          'OP_EQUALVERIFY', 'OP_CHECKSIG'])
 
     # create a P2SH address from a redeem script
--- Bitcoin_assignment/Task2.py
+++ Bitcoin_assignment/Task2.py
@@ -3,16 +3,36 @@ from bitcoinutils.utils import to_satoshis
 from bitcoinutils.keys import PrivateKey, P2pkhAddress, P2shAddress
 from bitcoinutils.transactions import Transaction, TxInput, TxOutput
 from bitcoinutils.script import Script
+from bitcoinutils.proxy import NodeProxy
+from bitcoinutils.constants import TYPE_ABSOLUTE_TIMELOCK
+# rpcuser=Alice
+# rpcpassword=DONT_USE_THIS_YOU_WILL_GET_ROBBED_8ak1gI25KFTvjovL3gAM967mies3E=
+import argparse
+from decimal import Decimal
 
 
 def main():
     # setup regtest network
     setup('regtest')
+    proxy = NodeProxy('alice', 'DONT_USE_THIS_YOU_WILL_GET_ROBBED_8ak1gI25KFTvjovL3gAM967mies3E=').get_proxy()
+    print(proxy.getblockcount())
+    parser = argparse.ArgumentParser(description='Bitcoin Script for Task I')
+    parser.add_argument('-private_key', help='Insert a public key')
+    parser.add_argument('--time_loc', type=int,
+                        help='Insert a parameter (block height/Unix Epoch time that funds should be locked')
+    parser.add_argument('-p2sh', help='Insert a p2sh')
+    parser.add_argument('-p2pkh', help='Insert a p2pkh')
 
     # the key that corresponds to the P2WPKH address
-    priv = PrivateKey('cNho8fw3bPfLKT4jPzpANTsxTsP8aTdVBD6cXksBEXt4KhBN7uVk')
+    # priv = PrivateKey('cNho8fw3bPfLKT4jPzpANTsxTsP8aTdVBD6cXksBEXt4KhBN7uVk')
+    args = parser.parse_args()
+    priv = args.private_key
+    p2sh = args.time_loc
+    p2pkh = args.p2pkh
+
     pub = priv.get_public_key()
 
+    proxy.list_unspent(p2sh)
     # the p2sh script and the corresponding address
     redeem_script = pub.get_segwit_address().to_script_pub_key()
     p2sh_addr = P2shAddress.from_script(redeem_script)
